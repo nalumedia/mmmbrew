7d8e4ec05faefdb4b4d0bd97abbdfa20b499b57f
Ryan Dranginis <ryan.dranginis@gmail.com>
2014-12-01 21:34:00 -0800

    added goog adsense

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :breweries
 
   get 'pages/home'
-  root 'pages#home'
+  root 'breweries#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
